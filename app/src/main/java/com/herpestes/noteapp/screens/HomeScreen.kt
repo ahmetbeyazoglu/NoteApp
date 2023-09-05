9ac7c83b4444e62782c2e2de57a2cb9ba1cb1d64
herpestes <ahmetbeyazoglu@gmail.com>
2023-09-05 23:32:02 +0300

    Search

--- app/src/main/java/com/herpestes/noteapp/screens/HomeScreen.kt
+++ app/src/main/java/com/herpestes/noteapp/screens/HomeScreen.kt
@@ -1,26 +1,62 @@
 package com.herpestes.noteapp.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.skydoves.colorpicker.compose.AlphaSlider
@@ -30,6 +66,9 @@ import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import com.herpestes.noteapp.database.NoteEntity
 import com.herpestes.noteapp.ui.theme.ubuntu
 
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class,
+    ExperimentalFoundationApi::class
+)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
 
@@ -155,12 +194,70 @@ fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
                     end = 12.dp
                 )
         ) {
-
+            val (searchOpen, setSearchOpen) = remember {
+                mutableStateOf(false)
+            }
+            val (searchQuery, setSearchQuery) = remember {
+                mutableStateOf("")
+            }
+            LaunchedEffect(searchQuery) {
+                viewModel.searchedNotes(searchQuery)
+            }
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
+                AnimatedVisibility(
+                    visible = !searchOpen,
+                    enter = fadeIn(tween(500)) + scaleIn(tween(500)),
+                    exit = fadeOut(tween(500)) + scaleOut(tween(500))
+                ) {
+                    Text(
+                        text = "My notes",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+                AnimatedVisibility(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(end = 8.dp),
+                    visible = searchOpen,
+                    enter = fadeIn(tween(500)) + scaleIn(tween(500)),
+                    exit = fadeOut(tween(500)) + scaleOut(tween(500))
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .clip(RoundedCornerShape(12.dp))
+                            .background(androidx.compose.material3.MaterialTheme.colorScheme.secondary)
+                            .height(50.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        TextField(value = searchQuery,
+                            onValueChange = {
+                                setSearchQuery(it)
+                            },
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Search")
+                            },
+                            colors = androidx.compose.material3.TextFieldDefaults.textFieldColors(
+                                containerColor = Color.Transparent,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent,
+                                focusedTextColor = Color.Transparent,
+                            )
+                        )
+                    }
+
+
+                }
 
 
             }
--- app/src/main/java/com/herpestes/noteapp/screens/HomeViewModel.kt
+++ app/src/main/java/com/herpestes/noteapp/screens/HomeViewModel.kt
@@ -13,41 +13,50 @@ import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class HomeViewModel : ViewModel(), KoinComponent {
-    private val repo: NoteRepo by inject()
-    private val _notes: MutableStateFlow<List<NoteEntity>> = MutableStateFlow(emptyList())
+class HomeViewModel:ViewModel(),KoinComponent {
+    private val repo:NoteRepo by inject()
+
+    private val _notes:MutableStateFlow<List<NoteEntity>> = MutableStateFlow(emptyList())
     val notes = _notes.asStateFlow()
-    private val _searchedNotes: MutableStateFlow<List<NoteEntity>?> = MutableStateFlow(null)
+
+    private val _searchedNotes:MutableStateFlow<List<NoteEntity>?> = MutableStateFlow(null)
     val searchedNotes = _searchedNotes.asStateFlow()
 
     init {
         getNotes()
     }
 
-    private fun getNotes() {
+    private fun getNotes(){
         viewModelScope.launch(Dispatchers.IO) {
-            repo.getNotes().collect { data ->
+            repo.getNotes().collect{data->
                 _notes.update { data }
             }
         }
     }
 
-    fun deleteNote(note: NoteEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
+    fun deleteNote(note:NoteEntity){
+        viewModelScope.launch(Dispatchers.IO){
             repo.deleteNote(note)
         }
     }
 
-    fun addNote(note: NoteEntity) {
-        viewModelScope.launch(Dispatchers.IO) { repo.addNote(note)
-        }
-    }
-    fun searchNotes(query: String){
-        if(query.isEmpty()){
-            _searchedNotes.update { null }
-        }else{
-            _searchedNotes.update { _notes.value.filter { it.note.contains(query) } }
+    fun addNote(note: NoteEntity){
+        viewModelScope.launch(Dispatchers.IO){
+            repo.addNote(note)
         }
     }
 
+    fun searchNotes(query:String){
+        if (query.isEmpty()){
+            _searchedNotes.update { null }
+        }else{
+            _searchedNotes.update {
+                _notes.value.filter { it.note.contains(query) }
+            }
+        }
+    }
+
+    fun searchedNotes(searchQuery: String) {
+
+    }
 }
